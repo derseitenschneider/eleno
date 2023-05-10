3d18b7cd6f673b21135e8aa80c04422991223bec
Brian Boy <info@derseitenschneider.ch>
2023-05-10 22:07:02 +0200

    new filestructure

--- src/utils/sortTodos.ts
+++
@@ -1,8 +0,0 @@
-import { TTodo } from '../types/types'
-
-export const compareDateTodos = (a: TTodo, b: TTodo) => {
-  const lessonA = +a.due?.split('-').join('')
-
-  const lessonB = +b.due?.split('-').join('')
-  return lessonA - lessonB
-}
