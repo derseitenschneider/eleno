1ab5bb03b15c3e5b424681c504696f0ea1482734
Brian Boy <info@derseitenschneider.ch>
2023-08-28 10:08:07 +0200

    Pull news-section

--- app/src/components/news/news.ts
+++ app/src/components/news/news.ts
@@ -2,7 +2,7 @@ import { TNews } from '../../types/types'
 
 const news: TNews[] = [
   {
-    date: '2023-08-26',
+    date: '2023-08-28',
     title: 'News Section im Dashboard',
     text: 'Neu gibt es diese News-Section hier im Dashboard. Hier wirst du laufend über alle wichtigen Updates und Neuerungen in Eleno informiert. Zudem hat das Dashboard einen leicht neuen Look bekommen für eine bessere Übersicht.',
   },
