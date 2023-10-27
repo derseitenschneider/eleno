99c67f3515dedaae6da15cb1567d5222cab19ba0
Brian Boy <info@derseitenschneider.ch>
2023-10-27 18:23:15 +0200

    Split scss into basic files

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './scss/global.scss'
+import './scss/main.scss'
 import { RouterProvider } from 'react-router-dom'
 
 import mainRouter from './router/mainRouter'
