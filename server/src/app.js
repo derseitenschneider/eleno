767228ed89ac0efde7b5a2f6732ef62903549ce2
Brian Boy <info@derseitenschneider.ch>
2023-11-01 22:30:26 +0100

    Prepared server for heroku

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const homeworkRouter = require('./routes/homeworkRouter');
+const compression = require('compression');
 const app = express();
 
 app.use(morgan('dev'));
+
+app.use(compression());
 app.use('/homework', homeworkRouter);
 
 module.exports = app;
