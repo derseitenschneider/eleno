d1d808b8fe9523e07ff1ccabd7db6196a7c2d91f
Brian Boy <info@derseitenschneider.ch>
2023-11-15 10:14:51 +0100

    Removed playwright. Finished share-homework feature

--- app/src/components/features/dashboard/news/news.ts
+++ app/src/components/features/dashboard/news/news.ts
@@ -2,6 +2,11 @@ import { TNews } from '../../../../types/types'
 
 const news: TNews[] = [
   {
+    date: '2023-11-15',
+    title: 'Neu: Hausaufgaben mit Schüler:innen teilen 🥳',
+    text: '<p>Jetzt kannst du mit zwei, drei Klicks die Hausaufgaben direkt mit deinen Schüler:innen teilen. Gehe dazu auf die Unterrichtsseite und klick in der erfassten Lektion auf den Teilen-Button. Dort kopierst du entweder den Link und verschickst ihn direkt. Oder du wählst einen der verfügbaren Kommunikationskanäle (Mail, SMS, Whatsapp, Threema und Telegram).</p><p><a href="https://manual.eleno.net/unterrichten/hausaufgaben-teilen/" target="_blank">Mehr Infos dazu findest du hier!</a></p>',
+  },
+  {
     date: '2023-10-21',
     title: 'Die Repertoire-Liste ist da!',
     text: `<p>Ab sofort kannst du für jede:n Schüler:in eine separate Repertoire-Liste führen. Du findest sie auf der Unterrichtsseite oben rechts.</p>
