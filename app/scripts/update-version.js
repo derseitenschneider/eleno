bcf37d44951d873ba23b7a60eed1372e4ae7508c
Brian Boy <info@derseitenschneider.ch>
2024-02-14 12:27:00 +0100

    Added update-version.js

--- app/scripts/update-version.js
+++ app/scripts/update-version.js
@@ -1,1 +1,38 @@
-console.log('test')
+;(() => {
+  try {
+    const pkg = require('../package.json')
+
+    // execSync('git remote show origin')
+
+    const { major, minor, patch } = parseVersionString(pkg.version)
+    const newVersion = `${major}.${minor}.${patch + 1}`
+
+    const pkgs = ['./package.json'].filter(fs.existsSync)
+
+    pkgs.map((pkgJson) => {
+      const pkgValues = JSON.parse(fs.readFileSync(pkgJson, 'utf8'))
+      pkgValues.version = newVersion
+      fs.writeFileSync(pkgJson, JSON.stringify(pkgValues, null, 2))
+      execSync(`git add ${pkgJson}`)
+      console.log(`updated ${pkgJson}`)
+    })
+
+    // const wpPluginFile =
+    //   './apps/wp.wp-deploy.hello/wp-content/plugins/deploy-wp/deploy-wp.php'
+    // const wpPluginFileContent = fs.readFileSync(wpPluginFile, 'utf8')
+    // const regex = /Version: [0-9\.]*/gm
+    // const versionOld = regex.exec(wpPluginFileContent)[0]
+    // const versionNew = `Version: ${newVersion}`
+    // const newWpPluginFileContnt = wpPluginFileContent.replace(
+    //   versionOld,
+    //   versionNew,
+    // )
+    // fs.writeFileSync(wpPluginFile, newWpPluginFileContnt)
+    // execSync(`git add ${wpPluginFile}`)
+    // console.log(`updated ${wpPluginFile}`)
+
+    console.log('application updated to', newVersion, 'version')
+  } catch (e) {
+    console.log('Version update failed', e)
+  }
+})()
