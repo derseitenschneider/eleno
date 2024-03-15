f7955595a2dc17ef7da546af12d2e26742023882
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-03-15 19:16:59 +0100

    chore(udpate-version.js): delete version

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -2,20 +2,11 @@
 const fs = require('fs');
 const parseVersionString = require('parse-version-string').default;
 const { execSync } = require('child_process');
-// const mainPackage = require('../package.json');
-// const appPackage = require('../app/package.json');
 
-// const fs = require('fs')
-// const parseVersionString = require('parse-version-string').default
-// const { execSync } = require('child_process')
 const pkg = require('../package.json');
 
 const updatePatch = () => {
   try {
-    // const pkg = require('../package.json')
-
-    // execSync('git remote show origin')
-
     const { major, minor, patch } = parseVersionString(pkg.version);
     const newVersion = `${major}.${minor}.${patch + 1}`;
 
@@ -29,20 +20,6 @@ const updatePatch = () => {
       execSync(`git add ${pkgJson}`);
       console.log(`updated ${pkgJson}`);
     });
-
-    // const wpPluginFile =
-    //   './apps/wp.wp-deploy.hello/wp-content/plugins/deploy-wp/deploy-wp.php'
-    // const wpPluginFileContent = fs.readFileSync(wpPluginFile, 'utf8')
-    // const regex = /Version: [0-9\.]*/gm
-    // const versionOld = regex.exec(wpPluginFileContent)[0]
-    // const versionNew = `Version: ${newVersion}`
-    // const newWpPluginFileContnt = wpPluginFileContent.replace(
-    //   versionOld,
-    //   versionNew,
-    // )
-    // fs.writeFileSync(wpPluginFile, newWpPluginFileContnt)
-    // execSync(`git add ${wpPluginFile}`)
-    // console.log(`updated ${wpPluginFile}`)
 
     console.log('application updated to', newVersion, 'version');
   } catch (e) {
