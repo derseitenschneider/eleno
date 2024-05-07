c3a28437803c70df96908b4645005cf729375c82
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-05-07 06:10:17 +0200

    wip: Fix overdue todos notification in sidebar and mobile nav

--- app/src/components/ui/SidebarElement.component.tsx
+++ app/src/components/ui/SidebarElement.component.tsx
@@ -28,11 +28,10 @@ export default function SidebarElement({
         title={name}
         to={to}
         target={target}
-        className={`${
-          !isActive
+        className={`${!isActive
             ? "text-foreground before:hidden"
             : "text-white after:hidden"
-        }
+          }
         align-center z-1 relative flex items-center gap-4 p-1.5 text-foreground
         before:absolute before:left-0 before:top-0 before:z-[-1] before:h-full
         before:w-full before:rounded-md before:bg-primary after:absolute after:left-0
@@ -42,22 +41,21 @@ export default function SidebarElement({
       >
         <div className='relative z-10 h-full w-full max-w-[22px] shrink-0 *:h-full *:w-full'>
           {icon}
-          <div
-            className={`${
-              notificationContent && notificationContent > 0 ? "hidden" : ""
-            } z-100 absolute bottom-0 right-0 flex
-            aspect-square max-h-[14px] max-w-[14px] translate-x-[25%] translate-y-[25%]
-            items-center justify-center rounded-full bg-warning`}
-          >
-            <span className='text-[10px] text-white'>
-              {notificationContent}
-            </span>
-          </div>
+          {notificationContent ? (
+            <div
+              className='z-100 absolute bottom-0 right-0 flex
+            aspect-square !size-[13px] translate-x-[25%] translate-y-[25%]
+            items-center justify-center rounded-full bg-warning'
+            >
+              <span className='text-[10px] text-white'>
+                {notificationContent}
+              </span>
+            </div>
+          ) : null}
         </div>
         <span
-          className={`${!sidebarOpen ? "hidden opacity-0" : "opacity-1"} ${
-            isActive ? "text-white" : ""
-          } whitespace-nowrap text-sm
+          className={`${!sidebarOpen ? "hidden opacity-0" : "opacity-1"} ${isActive ? "text-white" : ""
+            } whitespace-nowrap text-sm
           leading-none transition-opacity delay-500 duration-1000`}
         >
           {name}
--- app/src/layouts/navbarMobile/NavbarMobileItem.component.tsx
+++ app/src/layouts/navbarMobile/NavbarMobileItem.component.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef, useState } from "react"
-import { IoCheckboxOutline } from "react-icons/io5"
 import { NavLink, useLocation } from "react-router-dom"
 
 type NavbarMobileItemProps = {
@@ -14,25 +12,24 @@ export default function NavbarMobileItem({
 }: NavbarMobileItemProps) {
   const { pathname } = useLocation()
   const isActive = pathname === to && pathname !== "/"
+
   return (
     <li>
       <NavLink
         to={to}
-        className={`${
-          isActive ? "text-white" : "text-foreground"
-        } block size-[40px] p-1 relative`}
+        className={`${isActive ? "text-white" : "text-foreground"
+          } block size-[40px] p-1 relative`}
       >
         <span
-          className={`${
-            isActive ? "block" : "hidden"
-          } absolute top-0 left-0 size-full bg-primary z-[-1] rounded-sm`}
+          className={`${isActive ? "block" : "hidden"
+            } absolute top-0 left-0 size-full bg-primary z-[-1] rounded-sm`}
         />
         {icon}
         {notificationContent ? (
           <div
-            className={`${notificationContent < 0 ? "hidden" : "block"} z-100 absolute bottom-0 right-0 flex
-            aspect-square size-[18px] translate-x-[10%] translate-y-[10%]
-            items-center justify-center rounded-full bg-warning`}
+            className='z-100 absolute bottom-0 right-0 flex
+            aspect-square !size-[16px] items-center justify-center rounded-full 
+            bg-warning'
           >
             <span className='text-[10px] text-white'>
               {notificationContent}
