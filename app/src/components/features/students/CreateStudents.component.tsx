e3704c8c7c7fb306a59fa4b91907dc8aaf419740
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-06-26 07:00:42 +0200

    wip: Trying tanstack forms

--- app/src/components/features/students/CreateStudents.component.tsx
+++ app/src/components/features/students/CreateStudents.component.tsx
@@ -5,12 +5,20 @@ import { cn } from "@/lib/utils"
 import { useUser } from "@/services/context/UserContext"
 import type { Student, StudentPartial } from "@/types/types"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Plus } from "lucide-react"
+import { Plus, Trash2 } from "lucide-react"
 import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
+import { useForm } from "@tanstack/react-form"
 import { StudentInput, studentSchema } from "./StudentForm.component"
 import StudentFormRow from "./StudentFormRow.component"
 import { useCreateStudents } from "./useCreateStudents"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { SelectItem } from "@radix-ui/react-select"
 
 type CreateStudentsProps = {
   onSuccess: () => void
@@ -36,6 +44,14 @@ export default function CreateStudents({ onSuccess }: CreateStudentsProps) {
     isError: isErrorCreating,
   } = useCreateStudents()
 
+  const form = useForm({
+    defaultValues: {
+      students: [defaultStudent],
+    },
+    onSubmit({ value }) {
+      console.log(value)
+    },
+  })
   const [isError, setIsError] = useState(false)
   const [newStudents, setNewStudents] = useState<Array<RowStudent>>([
     {
@@ -44,26 +60,6 @@ export default function CreateStudents({ onSuccess }: CreateStudentsProps) {
     },
   ])
   const [numStudents, setNumStudents] = useState(1)
-
-  const form = useForm<StudentInput>({
-    resolver: zodResolver(studentSchema),
-    defaultValues: {
-      firstName: "",
-      lastName: "",
-      instrument: "",
-      dayOfLesson: null,
-      startOfLesson: "",
-      endOfLesson: "",
-      durationMinutes: null,
-      location: "",
-    },
-    mode: "all",
-    resetOptions: {
-      keepDirtyValues: true,
-      keepErrors: false,
-    },
-    shouldFocusError: true,
-  })
 
   const grid =
     "grid gap-2 items-stretch grid-cols-[8px_1fr_1fr_1fr_1fr_80px_80px_65px_1fr_16px]"
@@ -77,12 +73,6 @@ export default function CreateStudents({ onSuccess }: CreateStudentsProps) {
     setNumStudents(1)
   }
 
-  function deleteRow(tempId: number) {
-    if (newStudents.length === 1) return
-    setNewStudents((prev) =>
-      prev.filter((student) => student.tempId !== tempId),
-    )
-  }
   async function onSubmit(data) {
     if (!user) return
     console.log(data)
@@ -107,49 +97,227 @@ export default function CreateStudents({ onSuccess }: CreateStudentsProps) {
         <span className='text-sm ml-3'>Dauer</span>
         <span className='text-sm ml-3'>Unterrichtsort</span>
       </div>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <Form {...form}>
-          <div className='max-h-[75vh] no-scrollbar w-full overflow-auto p-1'>
-            {newStudents.map((newStudent, index) => (
-              <StudentFormRow
-                form={form}
-                onDeleteRow={deleteRow}
-                index={index}
-                grid={grid}
-                key={newStudent.tempId}
-                setStudents={setNewStudents}
-                student={newStudent}
-                setIsError={setIsError}
-              />
-            ))}
-          </div>
-          <div className='flex gap-4 items-center mt-4 justify-end'>
-            <div className='mr-auto flex items-center'>
-              <Input
-                type='number'
-                className='w-[60px]'
-                value={numStudents}
-                onChange={(e) => setNumStudents(Number(e.target.value))}
-              />
-              <Button
-                variant='ghost'
-                size='sm'
-                className='flex gap-1'
-                onClick={addStudents}
-              >
-                <Plus className='size-4 text-primary' />
-                <span>Zeilen hinzufügen</span>
-              </Button>
-            </div>
-            <Button size='sm' variant='outline' onClick={onSuccess}>
-              Abbrechen
-            </Button>
-            <Button size='sm' onClick={onSubmit}>
-              Speichern
-            </Button>
-          </div>
-        </Form>
-      </form>
+      <div className='max-h-[75vh] no-scrollbar w-full overflow-auto p-1'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            form.handleSubmit()
+          }}
+        >
+          <form.Field name='students' mode='array'>
+            {(field) => {
+              return (
+                <div>
+                  {field.state.value.map((_, i) => {
+                    return (
+                      <div className={grid} key={i}>
+                        <span>{i + 1}</span>
+                        <form.Field name={`students[${i}].firstName`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Input
+                                  value={subField.state.value}
+                                  onChange={(e) =>
+                                    subField.handleChange(e.target.value)
+                                  }
+                                />
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <form.Field name={`students[${i}].lastName`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Input
+                                  value={subField.state.value}
+                                  onChange={(e) =>
+                                    subField.handleChange(e.target.value)
+                                  }
+                                />
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <form.Field name={`students[${i}].instrument`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Input
+                                  value={subField.state.value}
+                                  onChange={(e) =>
+                                    subField.handleChange(e.target.value)
+                                  }
+                                />
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <form.Field name={`students[${i}].dayOfLesson`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Select
+                                  onValueChange={(e) => {
+                                    subField.handleChange(e)
+                                  }}
+                                  defaultValue={subField.state.value || ""}
+                                  // disabled={form.formState.isSubmitting}
+                                >
+                                  <SelectTrigger>
+                                    <SelectValue placeholder='Unterrichtstag' />
+                                  </SelectTrigger>
+                                  <SelectContent>
+                                    <SelectItem value='Montag'>
+                                      Montag
+                                    </SelectItem>
+                                    <SelectItem value='Dienstag'>
+                                      Dienstag
+                                    </SelectItem>
+                                    <SelectItem value='Mittwoch'>
+                                      Mittwoch
+                                    </SelectItem>
+                                    <SelectItem value='Donnerstag'>
+                                      Donnerstag
+                                    </SelectItem>
+                                    <SelectItem value='Freitag'>
+                                      Freitag
+                                    </SelectItem>
+                                    <SelectItem value='Samstag'>
+                                      Samstag
+                                    </SelectItem>
+                                    <SelectItem value='Sonntag'>
+                                      Sonntag
+                                    </SelectItem>
+                                    <SelectItem value='none'>-</SelectItem>
+                                  </SelectContent>
+                                </Select>
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <form.Field name={`students[${i}].startOfLesson`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Input
+                                  type='time'
+                                  value={subField.state.value}
+                                  onChange={(e) =>
+                                    subField.handleChange(e.target.value)
+                                  }
+                                />
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <form.Field name={`students[${i}].endOfLesson`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Input
+                                  type='time'
+                                  value={subField.state.value}
+                                  onChange={(e) =>
+                                    subField.handleChange(e.target.value)
+                                  }
+                                />
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <form.Field name={`students[${i}].durationMinutes`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Input
+                                  type='number'
+                                  value={subField.state.value}
+                                  onChange={(e) =>
+                                    subField.handleChange(e.target.value)
+                                  }
+                                />
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <form.Field name={`students[${i}].location`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <Input
+                                  value={subField.state.value}
+                                  onChange={(e) =>
+                                    subField.handleChange(e.target.value)
+                                  }
+                                />
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                        <Button
+                          variant='ghost'
+                          size='icon'
+                          className='w-fit justify-self-end'
+                          onClick={() => field.removeValue(i)}
+                        >
+                          <Trash2 className='size-4 text-warning' />
+                        </Button>
+                      </div>
+                    )
+                  })}
+                  <button
+                    onClick={() => field.pushValue(defaultStudent)}
+                    type='button'
+                  >
+                    Add person
+                  </button>
+                </div>
+              )
+            }}
+          </form.Field>
+          <Button type='submit'>Speichern</Button>
+        </form>
+        {/* {newStudents.map((newStudent, index) => ( */}
+        {/*   <StudentFormRow */}
+        {/*     form={form} */}
+        {/*     onDeleteRow={deleteRow} */}
+        {/*     index={index} */}
+        {/*     grid={grid} */}
+        {/*     key={newStudent.tempId} */}
+        {/*     setStudents={setNewStudents} */}
+        {/*     student={newStudent} */}
+        {/*     setIsError={setIsError} */}
+        {/*   /> */}
+        {/* ))} */}
+      </div>
+      <div className='flex gap-4 items-center mt-4 justify-end'>
+        <div className='mr-auto flex items-center'>
+          <Input
+            type='number'
+            className='w-[60px]'
+            value={numStudents}
+            onChange={(e) => setNumStudents(Number(e.target.value))}
+          />
+          <Button
+            variant='ghost'
+            size='sm'
+            className='flex gap-1'
+            onClick={addStudents}
+          >
+            <Plus className='size-4 text-primary' />
+            <span>Zeilen hinzufügen</span>
+          </Button>
+        </div>
+        <Button size='sm' variant='outline' onClick={onSuccess}>
+          Abbrechen
+        </Button>
+        <Button size='sm' onClick={onSubmit}>
+          Speichern
+        </Button>
+      </div>
     </div>
   )
 }
