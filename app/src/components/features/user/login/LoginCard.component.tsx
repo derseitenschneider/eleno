98d18f3c585f694bc25d4208fb176474b3aa8dd3
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-08-14 07:36:02 +0200

    chore: Minor mobile style changes

--- app/src/components/features/user/login/LoginCard.component.tsx
+++ app/src/components/features/user/login/LoginCard.component.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
@@ -6,25 +6,25 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import MiniLoader from "@/components/ui/MiniLoader.component"
-import { PasswordInput } from "@/components/ui/password-input"
-import { cn } from "@/lib/utils"
-import { loginSupabase } from "@/services/api/user.api"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useEffect } from "react"
-import { useForm } from "react-hook-form"
-import { Link } from "react-router-dom"
-import { z } from "zod"
-import WrapperCard from "./WrapperCard.component"
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import MiniLoader from '@/components/ui/MiniLoader.component'
+import { PasswordInput } from '@/components/ui/password-input'
+import { cn } from '@/lib/utils'
+import { loginSupabase } from '@/services/api/user.api'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { z } from 'zod'
+import WrapperCard from './WrapperCard.component'
 
 const loginSchema = z.object({
   email: z
     .string()
-    .min(1, { message: "E-Mail Adresse fehlt." })
-    .email({ message: "Keine gültige E-Mail Adresse." }),
-  password: z.string().min(1, { message: "Passwort fehlt." }),
+    .min(1, { message: 'E-Mail Adresse fehlt.' })
+    .email({ message: 'Keine gültige E-Mail Adresse.' }),
+  password: z.string().min(1, { message: 'Passwort fehlt.' }),
 })
 
 type TInput = z.infer<typeof loginSchema>
@@ -32,11 +32,11 @@ type TInput = z.infer<typeof loginSchema>
 export default function LoginCard() {
   const form = useForm<TInput>({
     defaultValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
     resolver: zodResolver(loginSchema),
-    mode: "onSubmit",
+    mode: 'onSubmit',
     resetOptions: {
       keepDirtyValues: true,
       keepErrors: false,
@@ -45,16 +45,16 @@ export default function LoginCard() {
   })
 
   useEffect(() => {
-    form.setFocus("email")
+    form.setFocus('email')
   }, [form])
 
   const onSubmit = async (data: TInput) => {
     try {
       await loginSupabase(data.email, data.password)
     } catch {
-      form.setFocus("email")
-      form.setError("root", {
-        message: "E-Mail Adresse und/oder Passwort ungültig.",
+      form.setFocus('email')
+      form.setError('root', {
+        message: 'E-Mail Adresse und/oder Passwort ungültig.',
       })
     }
   }
@@ -85,11 +85,12 @@ export default function LoginCard() {
                   </FormLabel>
                   <FormControl>
                     <Input
+                      type='email'
                       disabled={form.formState.isSubmitting}
                       className={cn(
                         form.formState.errors.email
-                          ? "border-warning"
-                          : "border-zinc-400",
+                          ? 'border-warning'
+                          : 'border-zinc-400',
                       )}
                       placeholder='E-Mail Adresse'
                       {...field}
@@ -112,8 +113,8 @@ export default function LoginCard() {
                       placeholder='Passwort'
                       className={cn(
                         form.formState.errors.password
-                          ? "border-warning"
-                          : "border-zinc-400",
+                          ? 'border-warning'
+                          : 'border-zinc-400',
                       )}
                       {...field}
                     />
