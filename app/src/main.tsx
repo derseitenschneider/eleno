13be0c4f4b953e3ee72d897df14576b68e253af6
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-08-21 07:44:09 +0200

    wip: Cleanup old files

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import './scss/main.scss'
 import './scss/tailwind.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
