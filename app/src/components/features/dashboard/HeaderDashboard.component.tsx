6b9693177f9282c85275962947044ab9b2106adc
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-08-24 05:39:32 +0200

    chore: Remove tsc from staging build

--- app/src/components/features/dashboard/HeaderDashboard.component.tsx
+++ app/src/components/features/dashboard/HeaderDashboard.component.tsx
@@ -1,39 +1,39 @@
-import { cn } from '@/lib/utils'
-import { useUser } from '../../../services/context/UserContext'
-import DarkmodeToggle from '../../ui/DarkmodeToggle.component'
+import { cn } from "@/lib/utils";
+import { useUser } from "../../../services/context/UserContext";
+import DarkmodeToggle from "../../ui/DarkmodeToggle.component";
 
 function HeaderDashboard() {
-  const { user } = useUser()
-  const mode = import.meta.env.VITE_MODE
+  const { user } = useUser();
+  const mode = import.meta.env.VITE_MODE;
 
   return (
     <header
       className={cn(
-        'px-3 py-6',
-        'md:p-4 md:pl-6',
-        'col-span-2 flex justify-between border-b border-hairline',
+        "px-3 py-6",
+        "md:p-4 md:pl-6",
+        "col-span-2 flex justify-between border-b border-hairline"
       )}
     >
-      <div className='left'>
+      <div>
         <h1>Dashboard</h1>
 
-        <div className=''>
-          {mode === 'demo' ? (
-            <span className=''>
+        <div>
+          {mode === "demo" ? (
+            <span className="">
               Willkommen und viel Spass beim Ausprobieren der Demo.
             </span>
           ) : (
-            <span className=''>
+            <span className="">
               Hi <b>{user?.firstName}</b>, willkommen bei Eleno!
             </span>
           )}
         </div>
       </div>
-      <div className=''>
+      <div className="">
         <DarkmodeToggle />
       </div>
     </header>
-  )
+  );
 }
 
-export default HeaderDashboard
+export default HeaderDashboard;
