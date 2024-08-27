b029652617906b659a9c0bdfe5060c510a40a4ef
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-08-27 21:04:09 +0200

    chore: Minor style changes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: false,
+        enabled: true,
       },
       manifest: lightManifestDesktop,
       minify: true,
