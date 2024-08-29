c1c9d00350f0b8838ea042d07dcc9846a0bd922f
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-08-29 07:28:33 +0200

    ci: Add service worker refresher for updates

---
+++ app/src/initializeServiceWorker.ts
@@ -0,0 +1,24 @@
+import { registerSW } from 'virtual:pwa-register'
+
+// This function will be called when the app starts
+export function initializeServiceWorker() {
+  const updateSW = registerSW({
+    onNeedRefresh() {
+      // This function is called when a new service worker is available
+      if (confirm('A new version of the app is available. Update now?')) {
+        updateSW(true)
+      }
+    },
+    onOfflineReady() {
+      console.log('App is ready for offline use.')
+    },
+  })
+
+  // Periodically check for updates
+  setInterval(
+    () => {
+      updateSW()
+    },
+    60 * 60 * 1000,
+  ) // Check every hour, adjust as needed
+}
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './styles/tailwind.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { initializeServiceWorker } from './initializeServiceWorker'
 
 import { RouterProvider } from 'react-router-dom'
 
 import mainRouter from './router/mainRouter'
 
 const queryClient = new QueryClient()
+
+initializeServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
