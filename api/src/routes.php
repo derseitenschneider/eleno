cb9f080930fea79c42cf72f57d5a20aefebeac16
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-09-29 10:11:54 +0200

    feat: Add catch all route and automatic deploy

--- api/src/routes.php
+++ api/src/routes.php
@@ -2,13 +2,26 @@
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 use App\Controllers\HomeworkController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\RequestInterface as Request;
 
 return function (App $app) {
     $app->group(
         '/homework', function (RouteCollectorProxy $group) {
-            $group->get('/{entity_id}/{homework_key}', [HomeworkController::class, 'getHomework']);
+            $group->get(
+                '/{entity_id}/{homework_key}',
+                [HomeworkController::class, 'getHomework']
+            );
         }
     );
 
-    // Add more route groups here as needed
+    // Catch all route
+    $app->any(
+        '{route:.*}', function (Request $request, Response $response) {
+            return $response
+                ->withHeader('Location', 'https://eleno.net')
+                ->withStatus(302);
+        }
+    );
+
 };
