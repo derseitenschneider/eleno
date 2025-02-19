13030f4d2106dabfc7e4a3fb14256207d3471579
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2025-02-19 05:53:55 +0100

    feat: Minor changes in reset user script

--- api/scripts/reset-user.php
+++ api/scripts/reset-user.php
@@ -11,12 +11,10 @@ use App\Services\Stripe\StripeAPIService;
 use App\Services\SupabaseService;
 use Stripe\Stripe;
 
-// Load configuration (adapt to your config loading method)
 $config    = new Config();
 $supabase  = new SupabaseService( $config );
 $stripeApi = new StripeAPIService( $config );
 
-// Get user ID (you might want to pass this as a command-line argument)
 $userId = $argv[1] ?? '13c1e634-0906-4c30-8622-c786957553ae';
 
 if ( $userId === null ) {
@@ -36,11 +34,10 @@ $stripeSubscription = $supabase->get(
 );
 
 $stripeSubscriptionId = $stripeSubscription['data'][0]['stripe_subscription_id'];
-echo "Retrieved subscriptionId: {$stripeSubscriptionId}\n";
 
 // Reset user data in Supabase
 $supabase->patch(
-	endpoint: 'stripe_subscriptions', // The table where user data is stored
+	endpoint: 'stripe_subscriptions',
 	data: array(
 		'stripe_subscription_id' => null,
 		'stripe_invoice_id'      => null,
@@ -51,11 +48,11 @@ $supabase->patch(
 		'is_lifetime'            => false,
 		'subscription_status'    => 'trial',
 	),
-	query: array( 'user_id' => 'eq.' . $userId ) // Assuming 'id' is your user ID column
+	query: array( 'user_id' => 'eq.' . $userId )
 );
 
 if ( empty( $stripeSubscriptionId ) ) {
-	echo "User {$userId} reset successfully.\n No subscription found therefore no stripe action required.";
+	echo "User {$userId} reset successfully.\nNo subscription found therefore no stripe action required.";
 	exit;
 }
 
