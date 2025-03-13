2ca565a03845871300cfdab67294b602b546fa64
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2025-03-13 06:46:59 +0100

    ci: switch to html reporter for ci, add debug ls for action

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'list',
+  reporter: process.env.CI ? 'html' : 'list',
   use: {
     baseURL: process.env.LOCAL_TEST
       ? 'http://localhost:5173'
