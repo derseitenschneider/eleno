830e52173bb0c7f3db256fbfd8ed82b198c84478
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2025-03-21 06:24:46 +0100

    ci: tried and dismissed testing with localhost in ci

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? 'html' : 'list',
+  webServer: {
+    command: 'npm run dev:prod',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
   use: {
     baseURL: process.env.LOCAL_TEST
       ? 'http://localhost:5173'
