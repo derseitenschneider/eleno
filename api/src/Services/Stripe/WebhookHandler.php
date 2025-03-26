dfdd4dd9fe2f95925168c2323e366cea55fe0af0
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2025-03-26 07:28:25 +0100

    wip: test monthly expired canceled

--- api/src/Services/Stripe/WebhookHandler.php
+++ api/src/Services/Stripe/WebhookHandler.php
@@ -131,7 +131,6 @@ class WebhookHandler {
 					)
 				);
 				$this->repository->cancelSubscription( $subscriptionId );
-				$this->repository->resetFailedPaymentAttempts( $stripeCustomer );
 				$this->stripeAPI->cancelSubscription( $subscriptionId );
 			}
 		}
