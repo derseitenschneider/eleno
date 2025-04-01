f56444f1f5359cda4e798ff2937899ad6c981c14
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2025-04-01 20:22:54 +0200

    test: Share homework

--- app/tests/share-homework/share-homework.spec.ts
+++ app/tests/share-homework/share-homework.spec.ts
@@ -2,16 +2,25 @@ import { test as setup, expect } from '@playwright/test'
 import { TestUser } from '../utils/TestUser'
 
 setup(
-  'create a trial user, run checkout fixture and activate',
+  'create user, student and lesson, navigate to homework api and expect homework to be there.',
   async ({ page }) => {
-    // Setup test data.
+    // Setup test user.
     const testUser = new TestUser({ userflow: 'share-homework' })
     await testUser.init()
+
+    // Create test lesson.
     const lesson = await testUser.createLesson()
+
+    // Navigate to homework api.
     const studentId = lesson?.studentId
     const homeworkKey = lesson?.homeworkKey
     await page.goto(
       `https://api.test.eleno.net/homework/${studentId}/${homeworkKey}`,
+    )
+
+    // Expect homework heading to be visible.
+    await expect(page.getByTestId('homework-heading')).toContainText(
+      'Hausaufgaben vom',
     )
   },
 )
